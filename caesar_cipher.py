889b5820cd20d5c5cdee445f1bc7965f3f491730
kbirgelis <149561984+kbirgelis@users.noreply.github.com>
2023-11-15 10:17:18 +0200

    Update caesar_cipher.py

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,22 +1,30 @@
-a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alfabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-def e(t, o):
-  r = ""
-  for c in t:
-    if (a.find(c) == -1):
-      r += c
-    else:
-      r += (a[(a.find(c) + o) % len(a)])
-  return r
 
-def d(t, o):
-  r = ""
-  for c in t:
-    if (a.find(c) == -1):
-      r += c
-    else:
-      r += (a[(a.find(c) - o) % len(a)])
-  return r
+def encrypt(teksts, rotacija):  #Definē "Encryption" funkciju.
+  result = ""
+  for c in teksts:  #Cikls, kas tiek izmantots, lai pārveidotu burtus.
+    if (alfabets.find(c) == -1):  #Ja atrod c, tad tiek atņemts burts.
+      result += c
+    else:  #Ja a neatrod c, tad tiek pievienots viens burts.
+      result += (alfabets[(alfabets.find(c) + rotacija) % len(alfabets)])
+  return result
+
+
+
+
+def decrypt(teksts, rotacija):  #Definē burtu d.
+  result = ""
+  for c in teksts:  #Cikls, kas tiek izmantots, lai pārveidotu burtus.
+    if (alfabets.find(c) == -1):  #Ja a atrod c, tad tiek atņemts viens burts.
+      result += c
+    else:  #Ja a neatrod c, tad tiek atgriezta iepriekšējā pozīcija.
+      result += (alfabets[(alfabets.find(c) - rotacija) % len(alfabets)])
+  return result
+
+
+
+
 
 w = """
 1. Encrypt text
@@ -25,14 +33,23 @@ w = """
 Choose mode: """
 mode = int(input(w))
 
-if mode == 1:
+
+
+
+
+if mode == 1:  #Ja ir izvēlēts 1. režīms, tad tiek printēts
   text = input("Enter the text: ")
   rotation = int(input("Enter the rotation: "))
-  print("Encrypted: " + e(text, rotation))
-elif mode == 2:
+  print("Encrypted: " + encrypt(text, rotation))
+elif mode == 2:  #Ja ir izvēlēts 2. režīms, tad tiek printēta atbilde, ja nē, tad tiek izvēlēts nākošais režīms
   text = input("Enter the text: ")
   rotation = int(input("Enter the rotation: "))
-  print("Decrypted: " + d(text, rotation))
-elif mode == 3:
+  print("Decrypted: " + decrypt(text, rotation))
+elif mode == 3:  #Ja izvēlēts 3. režīms, tad printēt.
+  text = input("Enter the text: ")
   print("Bruteforcing...")
-  print("But I don't know how to do it, sorry ¯\_(ツ)_/¯")
+  for n in range(26):
+    print(str(n+1) + ": " + decrypt(text, n))
+    
+ 
+  
